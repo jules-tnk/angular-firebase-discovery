9c6ca776ed9d48c48aa0ff88340d0a5356e73982
Kibalo Jules Tinaka <julestnk.dev@gmail.com>
2023-03-15 09:50:31 +0100

    Add drag and drop on task items

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import {Task} from "./models/task";
+import {CdkDragDrop, transferArrayItem} from "@angular/cdk/drag-drop";
 
 @Component({
   selector: 'app-root',
@@ -14,5 +15,31 @@ export class AppComponent {
     {title: 'Task 2', description: 'Description 2'},
     {title: 'Task 3', description: 'Description 3'},
   ];
+
+  inProgress: Task[] = [];
+
+  done: Task[] = [];
+
+  editTask(list: string, task: Task): void {
+    console.log('Edit Task');
+  }
+
+  drop(event: CdkDragDrop<Task[]>): void {
+    if (event.previousContainer === event.container){
+      return;
+    }
+
+    if (!event.container.data || !event.previousContainer.data) {
+      return;
+    }
+
+    transferArrayItem(
+      event.previousContainer.data,
+      event.container.data,
+      event.previousIndex,
+      event.currentIndex
+    );
+
+  }
 
 }
